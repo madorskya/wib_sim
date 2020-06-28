78c67221b11dc3135d2b8a5ea77cb39821d3501d
Alexander Madorsky <16050565+madorskya@users.noreply.github.com>
2020-06-28 17:38:43 -0400

    fixed problem with rxbytealign reporting misalignment, by enabling alignment to pos and neg commas

--- wib_sim/wib_sim.srcs/sources_1/ip/coldata_rx/coldata_rx_stub.vhdl
+++ wib_sim/wib_sim.srcs/sources_1/ip/coldata_rx/coldata_rx_stub.vhdl
@@ -1,7 +1,7 @@
 -- Copyright 1986-2020 Xilinx, Inc. All Rights Reserved.
 -- --------------------------------------------------------------------------------
 -- Tool Version: Vivado v.2020.1 (lin64) Build 2902540 Wed May 27 19:54:35 MDT 2020
--- Date        : Sat Jun 27 19:55:08 2020
+-- Date        : Sun Jun 28 15:13:10 2020
 -- Host        : endcap-tf1.phys.ufl.edu running 64-bit CentOS Linux release 7.8.2003 (Core)
 -- Command     : write_vhdl -force -mode synth_stub
 --               /home/madorsky/cernbox/projects/vivado/dune_wib/wib_sim/wib_sim.srcs/sources_1/ip/coldata_rx/coldata_rx_stub.vhdl
