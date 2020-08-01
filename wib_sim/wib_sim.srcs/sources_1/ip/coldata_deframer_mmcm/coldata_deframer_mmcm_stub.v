5b02a087b4c850219f9ad4d594158ce0700fe110
Alexander Madorsky <16050565+madorskya@users.noreply.github.com>
2020-07-31 22:50:12 -0400

    adding missing sources

---
+++ wib_sim/wib_sim.srcs/sources_1/ip/coldata_deframer_mmcm/coldata_deframer_mmcm_stub.v
@@ -0,0 +1,22 @@
+// Copyright 1986-2020 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2020.1 (lin64) Build 2902540 Wed May 27 19:54:35 MDT 2020
+// Date        : Mon Jul 13 11:27:09 2020
+// Host        : endcap-tf1.phys.ufl.edu running 64-bit CentOS Linux release 7.8.2003 (Core)
+// Command     : write_verilog -force -mode synth_stub
+//               /home/madorsky/cernbox/projects/vivado/dune_wib/wib_sim/wib_sim.srcs/sources_1/ip/coldata_deframer_mmcm/coldata_deframer_mmcm_stub.v
+// Design      : coldata_deframer_mmcm
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xczu9eg-ffvb1156-2-e
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+module coldata_deframer_mmcm(clk_out1, reset, locked, clk_in1)
+/* synthesis syn_black_box black_box_pad_pin="clk_out1,reset,locked,clk_in1" */;
+  output clk_out1;
+  input reset;
+  output locked;
+  input clk_in1;
+endmodule
---
+++ wib_sim/wib_sim.srcs/sources_1/ip/coldata_deframer_mmcm/coldata_deframer_mmcm_stub.vhdl
@@ -0,0 +1,31 @@
+-- Copyright 1986-2020 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2020.1 (lin64) Build 2902540 Wed May 27 19:54:35 MDT 2020
+-- Date        : Mon Jul 13 11:27:09 2020
+-- Host        : endcap-tf1.phys.ufl.edu running 64-bit CentOS Linux release 7.8.2003 (Core)
+-- Command     : write_vhdl -force -mode synth_stub
+--               /home/madorsky/cernbox/projects/vivado/dune_wib/wib_sim/wib_sim.srcs/sources_1/ip/coldata_deframer_mmcm/coldata_deframer_mmcm_stub.vhdl
+-- Design      : coldata_deframer_mmcm
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xczu9eg-ffvb1156-2-e
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity coldata_deframer_mmcm is
+  Port ( 
+    clk_out1 : out STD_LOGIC;
+    reset : in STD_LOGIC;
+    locked : out STD_LOGIC;
+    clk_in1 : in STD_LOGIC
+  );
+
+end coldata_deframer_mmcm;
+
+architecture stub of coldata_deframer_mmcm is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "clk_out1,reset,locked,clk_in1";
+begin
+end;
