bd27df10b59830fac5a330ab8e0353e4af3e9ba5
U-CERN\madorsky <madorsky@uf-eng-srv-1.cern.ch>
2020-09-21 18:23:59 +0200

    changed fake master frequency to 156.25, because CDR doubles the frequency

--- wib_sim/wib_sim.srcs/sim_1/wib/timing_master_fake.sv
+++ wib_sim/wib_sim.srcs/sim_1/wib/timing_master_fake.sv
@@ -5,12 +5,12 @@ module timing_master_fake
 	output clk240
 );
 
-    wire clk312p5;
-    assign tx_timing = clk312p5;
+    wire clk156p25;
+    assign tx_timing = clk156p25;
 
     standalone_50m_mmcm mcmm50
     (
-        .clk_out1 (clk312p5),
+        .clk_out1 (clk156p25),
         .clk_out2 (clk240),
         .reset    (1'b0),
         .locked   (),
