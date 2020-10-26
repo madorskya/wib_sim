ed56ac63b4527c31f3a0e37951c727a6f2bc2672
U-CERN\madorsky <madorsky@uf-eng-srv-1.cern.ch>
2020-10-26 23:16:27 +0100

    added data_type outputs to frame_builder, needed by felix

--- wib_sim/wib_sim.srcs/sources_1/ip/ila_1/ila_1_stub.v
+++ wib_sim/wib_sim.srcs/sources_1/ip/ila_1/ila_1_stub.v
@@ -1,7 +1,7 @@
 // Copyright 1986-2020 Xilinx, Inc. All Rights Reserved.
 // --------------------------------------------------------------------------------
 // Tool Version: Vivado v.2020.1.1_AR73018 (win64) Build 2960000 Wed Aug  5 22:57:20 MDT 2020
-// Date        : Thu Oct 15 19:30:29 2020
+// Date        : Mon Oct 26 21:57:35 2020
 // Host        : uf-eng-srv-1 running 64-bit major release  (build 9200)
 // Command     : write_verilog -force -mode synth_stub
 //               c:/Users/madorsky/github/wib_sim/wib_sim/wib_sim.srcs/sources_1/ip/ila_1/ila_1_stub.v
@@ -14,11 +14,13 @@
 // The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
 // Please paste the declaration into a Verilog source file or add the file as an additional source.
 (* X_CORE_INFO = "ila,Vivado 2020.1.1_AR73018" *)
-module ila_1(clk, probe0, probe1, probe2, probe3)
-/* synthesis syn_black_box black_box_pad_pin="clk,probe0[31:0],probe1[31:0],probe2[3:0],probe3[3:0]" */;
+module ila_1(clk, probe0, probe1, probe2, probe3, probe4, probe5)
+/* synthesis syn_black_box black_box_pad_pin="clk,probe0[31:0],probe1[31:0],probe2[3:0],probe3[3:0],probe4[1:0],probe5[1:0]" */;
   input clk;
   input [31:0]probe0;
   input [31:0]probe1;
   input [3:0]probe2;
   input [3:0]probe3;
+  input [1:0]probe4;
+  input [1:0]probe5;
 endmodule
