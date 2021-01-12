5d286213b5f9eb165f14bc23523791aa50b63fd5
U-CERN\madorsky <madorsky@uf-eng-srv-1.cern.ch>
2021-01-12 17:45:29 +0100

    adding missing files

---
+++ wib_zu9cg/wib_zu9cg.srcs/sources_1/ip/mmcm_40m/mmcm_40m_stub.v
@@ -0,0 +1,22 @@
+// Copyright 1986-2020 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2020.1.1_AR73018 (win64) Build 2960000 Wed Aug  5 22:57:20 MDT 2020
+// Date        : Sun Jan 10 19:06:25 2021
+// Host        : uf-eng-srv-1 running 64-bit major release  (build 9200)
+// Command     : write_verilog -force -mode synth_stub
+//               c:/github/wib_sim/wib_zu9cg/wib_zu9cg.srcs/sources_1/ip/mmcm_40m/mmcm_40m_stub.v
+// Design      : mmcm_40m
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xczu9cg-ffvb1156-1-e
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+module mmcm_40m(clk_out1, reset, locked, clk_in1)
+/* synthesis syn_black_box black_box_pad_pin="clk_out1,reset,locked,clk_in1" */;
+  output clk_out1;
+  input reset;
+  output locked;
+  input clk_in1;
+endmodule
