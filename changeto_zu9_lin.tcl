9950f291571b1feee64a1d98ccb5e2c14a51697a
Alexander Madorsky <16050565+madorskya@users.noreply.github.com>
2022-04-05 17:47:14 -0400

    maintenance

--- changeto_zu9_lin.tcl
+++
@@ -1,3 +0,0 @@
-save_project_as wib_zu9eg /home/madorsky/github/wib_sim/wib_zu9eg -exclude_run_results -force
-set_property part xczu9eg-ffvb1156-1-e [current_project]
-
--- changeto_zu9_win.tcl
+++
@@ -1,2 +0,0 @@
-save_project_as wib_zu9eg C:/github/wib_sim/wib_zu9eg -exclude_run_results -force
-set_property part xczu9eg-ffvb1156-1-e [current_project]
